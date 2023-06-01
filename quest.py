9d4533c5d75f6c33ce3dab66f8f51be7b2d655fa
JVelezFD <jvelez117@gmail.com>
2023-06-01 13:59:08 -0400

    added draw function for scenery, character, and setup game

--- quest.py
+++ quest.py
@@ -6,11 +6,11 @@ GRID_SIZE = 50
 
 WIDTH = GRID_WIDTH * GRID_SIZE 
 HEIGHT = GRID_HEIGHT * GRID_SIZE
-map = ["WWWWWWWWWWWWWWWW", 
+MAP = [ "WWWWWWWWWWWWWWWW", 
         "W              W",
         "W              W",
         "W   W KG       W",
-        "W  WWWWWWWWW  W",
+        "W  WWWWWWWWW   W",
         "W              W",
         "W      P       W",
         "W  WWWWWWWWW   W",
@@ -22,11 +22,37 @@ map = ["WWWWWWWWWWWWWWWW",
 
 def screen_coords(x, y):
     return (x * GRID_SIZE, y * GRID_SIZE)
+
+def setup_game():
+    global player
+    player = Actor("player", anchor=("left", "top"))
+    for y in range(GRID_HEIGHT):
+        for x in range(GRID_WIDTH):
+            square = MAP[y][x]
+            if square == "P":
+                player.pos = screen_coords(x,y)
+
 def draw_background():
     for y in range(GRID_HEIGHT):
         for x in range(GRID_WIDTH):
-            screen.blit("floor 1", screen_coords(x,y))
+            screen.blit("floor1", screen_coords(x,y))
+
+def draw_scenery():
+    for y in range(GRID_HEIGHT):
+        for x in range (GRID_WIDTH):
+            square = MAP[y][x]
+            if square == "W":
+                screen.blit("wall",screen_coords(x, y))
+            elif square == "D":
+                screen.blit("door", screen_coords(x, y))
+
+def draw_actors():
+    player.draw()
+
 def draw():
     draw_background() 
+    draw_scenery()
+    draw_actors()
 
+setup_game()
 pgzrun.go()
